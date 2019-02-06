7d9ded2f64c9f3c4e1335ff1ffb5269e3c29296f
tanakaworld <yutaro.tanaka.world@gmail.com>
2019-02-07 01:08:12 +0900

    simplify

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-const fs = require('fs');
 const puppeteer = require('puppeteer');
+
+const pdfUrl = 'https://proff.io/p/tanakaworld';
 
 async function printPDF() {
     const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
-    await page.goto('https://proff.io/p/tanakaworld', {waitUntil: 'networkidle0'});
-    const pdf = await page.pdf({format: 'A4'});
+    await page.goto(pdfUrl, {waitUntil: 'networkidle0'});
+    const pdf = await page.pdf({path: `./pdf/test.pdf`, format: 'A4'});
 
     await browser.close();
     return pdf;
 }
 
 (async () => {
-    const pdf = await printPDF();
-    fs.writeFile('./pdf/test.pdf', pdf, (error) => {
-        if (error) {
-            console.error('error:', error);
-            return;
-        }
-        console.log('completed');
-    });
+    await printPDF();
 })();
